614a7f060b7cb35776e4915d5cc8c3f9dc195be1
Petra Popovic <ppopovic0411@gmail.com>
2026-02-09 20:44:34 +0100

    ispravljen link i forma

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -10,7 +10,7 @@ function Restaurant() {
             <img className={styles.lokacija} src={lokacija}></img>
             <p className={styles.restoran}>Restoran</p>
             <p className={styles.mesecev}>Mesečev konak</p>
-            <a href='https://google.com/maps/place/Месечев+конак/data=!4m2!3m1!1s0x0:0xb8810a1043e45c6b?sa=X&ved=1t:2428&ictx=111' target="_blank" className={styles.mapa}>Prikaži lokaciju na mapi</a>
+            <a href='https://www.google.com/maps/search/?api=1&query=Mesecev+konak' target="_blank" className={styles.mapa}>Prikaži lokaciju na mapi</a>
         </div>
         <img className={styles.mesecevslika} src={mesecev}></img>
     </div>
